92764893204c42692772f266b8a3f2ed464876a4
mallikarjun3739 <mallikarjun16455@gmail.com>
2022-08-31 13:27:58 +0530

    nodemmmm

---
+++ index.js
@@ -0,0 +1,15 @@
+// const http = require('http');
+
+// http.createServer((req,res)=>{
+//     res.write("<h1> this is akshay and mallikarjun</h1>");
+//     res.end();
+// }).listen(5000);
+
+// const http = require('http');
+
+// function datacontrol(req,res){
+
+//     res.write("<h1>this is akshay and mallikarjun</h1>");
+//     res.end();
+// }
+// http.createServer(datacontrol).listen(5000);
